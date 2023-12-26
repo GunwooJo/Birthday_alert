cd7334adb9778237f7e158309810a955c99d00d8
GunwooJo <kanggi1997@naver.com>
2023-12-26 18:29:20 +0900

    feat: 생년월일 정보 입력

--- components/AddFriends.tsx
+++ components/AddFriends.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { ScrollView, Text, TextInput, StyleSheet } from "react-native";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 function AddFriends() {
 
-    const [name, setName] = useState('');
+    const [name, setName] = useState<string>('');
+    const [birthday, setBirthday] = useState<Date>(new Date())
 
     const styles = StyleSheet.create({
         input: {
@@ -17,12 +19,21 @@ function AddFriends() {
     return (
         <ScrollView>
             <Text>친구정보 입력</Text>
+            
             <TextInput  
                 style={styles.input}
                 value={name}
-                onChangeText={newText => setName(newText)}
+                onChangeText={(newText: string) => setName(newText)}
                 placeholder="친구이름 입력"
             />
+
+            <Text>생년월일</Text>
+            <DateTimePicker 
+                value={birthday}
+                mode="date"
+                display="default"
+            />
+            
         </ScrollView>
     );
 }

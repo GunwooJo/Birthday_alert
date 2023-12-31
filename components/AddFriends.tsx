d68f41bbe1ff79c244d030782a4377f82ece09f4
GunwooJo <kanggi1997@naver.com>
2023-12-31 15:34:32 +0900

    springboot setting

--- components/AddFriends.tsx
+++ components/AddFriends.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { ScrollView, Text, TextInput, StyleSheet, Button } from "react-native";
+import { ScrollView, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
-import { Input, Icon } from '@rneui/themed';
+import { Input, Button, Overlay } from '@rneui/themed';
 
 function AddFriends() {
 
     const [name, setName] = useState<string>('');
     const [birthday, setBirthday] = useState<Date>(new Date());
+    const [overlayVisible, setOverlayVisible] = useState(false);
 
     const styles = StyleSheet.create({
         input: {
@@ -15,12 +16,19 @@ function AddFriends() {
           borderWidth: 1,
           padding: 10,
         },
+        button: {
+            margin: 10,
+        },
       });
 
     const onDateChange = (event: DateTimePickerEvent, selectedDate: Date | undefined) => {
         const currentDate = selectedDate || birthday;
         setBirthday(currentDate);
     };
+
+    const toggleOverlay = () => {
+        setOverlayVisible(!overlayVisible);
+      };
 
     return (
         <ScrollView>
@@ -40,6 +48,18 @@ function AddFriends() {
                 display="spinner"
                 onChange={onDateChange}
             />
+
+            <Button
+                title="알림 지정"
+                onPress={toggleOverlay}
+                buttonStyle={styles.button}
+            />
+
+            <Overlay isVisible={overlayVisible} onBackdropPress={toggleOverlay}>
+                <Text>이것이 오버레이다!이것이 오버레이다!이것이 오버레이다!이것이 오버레이다!이것이 오버레이다!</Text>
+                <Text>아이브 레이 아니다!</Text>
+            </Overlay>
+
         </ScrollView>
     );
 }
